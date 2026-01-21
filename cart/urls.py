fd64b7647a4fe901699807922bc76f8e394fecda
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-01-21 14:07:40 +0500

    django

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path,include
-
+from django.urls import path
 from . import views
-from .views import create_user
 
-urlpatterns=[
-    path('',views.index ,name='index'),
-    path('detail/<int:User_id>/',views.detail,name='detail'),
-    path('create_user/',views.create_user,name='create_user')
+urlpatterns = [
+    path('', views.ListView.as_view(), name='index'),
+    # Yangi foydalanuvchi qo'shish uchun (argumentlarsiz)
+    path('create_user/', views.CreateView.as_view(), name='create_user'),
+    # Tahrirlash uchun (pk bilan)
+    path('edit_user/<int:pk>/', views.CreateView.as_view(), name='edit_user'),
+    # Detail sahifasi uchun (pk yoki User_id)
+    path('detail/<int:User_id>/', views.detail, name='detail'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,33 +1,87 @@
 from pydoc import describe
 
 from django.shortcuts import render,redirect, get_object_or_404
-
+from django.views import View
 # Create your views here.
 from .models import Users
-def index(request):
-    data=Users.objects.all()
-    context={
-        'users':data,
-    }
-    return render(request,'index.html',context)
-def detail(request, pk):
-    user = Users.objects.get(id=pk)
-    return render(request, 'detail.html', {'user': user})
-def create_user(request):
+class ListView(View):
+    def get(self,request):
+        user=Users.objects.all()
+        return render(request,'index.html',{'user':user})
 
-    if request.method=='POST':
-        name=request.POST.get('name')
-        lastname=request.POST.get('lastname')
-        ID_kart=request.POST.get('ID_kart')
-        tugulgan_kun=request.POST.get('tugulgan_kun')
-        desc=request.POST.get('desc')
+class CreateView(View):
+    def get(self,request,pk):
+        user=Users.objects.filter(id=pk).first()
+        return render(request,'create_user.html',{'create_user':user})
+    def post(self,request):
+        name = request.POST.get('name')
+        lastname = request.POST.get('lastname')
+        ID_kart = request.POST.get('ID_kart')
+        tugulgan_kun = request.POST.get('tugulgan_kun')
+        desc = request.POST.get('desc')
 
-        Users.objects.create(
+        user=Users.objects.create(
             name=name,
             lastname=lastname,
             ID_kart=ID_kart,
             tugulgan_kun=tugulgan_kun,
             desc=desc
         )
+        user.save()
         return redirect('index')
-    return render(request,'create_user.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def index(request):
+#     data=Users.objects.all()
+#     context={
+#         'users':data,
+#     }
+#     return render(request,'index.html',context)
+# def detail(request, User_id):
+#     user = Users.objects.get(id=User_id)
+#     return render(request, 'detail.html', {'user': user})
+# def create_user(request):
+#     if request.method=='POST':
+#         name=request.POST.get('name')
+#         lastname=request.POST.get('lastname')
+#         ID_kart=request.POST.get('ID_kart')
+#         tugulgan_kun=request.POST.get('tugulgan_kun')
+#         desc=request.POST.get('desc')
+#
+#         Users.objects.create(
+#             name=name,
+#             lastname=lastname,
+#             ID_kart=ID_kart,
+#             tugulgan_kun=tugulgan_kun,
+#             desc=desc
+#         )
+#         return redirect('index')
+#     return render(request,'create_user.html')
